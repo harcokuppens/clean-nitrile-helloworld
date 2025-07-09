0f7b4d04b945a5875e8773a699ebe36953f723ba
Harco Kuppens <h.kuppens@gmail.com>
2025-07-09 23:28:08 +0200

    improved path in env.bash

--- env.bash
+++ env.bash
@@ -1,4 +1,5 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-export PATH="$script_dir/bin-nitrile:$PATH"
-PLATFORM_ARCH=$(nitrile-target)
-export PATH="$script_dir/bin-nitrile/prj/$PLATFORM_ARCH:$PATH"
+PLATFORM_ARCH=$($script_dir/bin-nitrile/nitrile-target)
+# note: bin-nitrile must be in the PATH earlier to prefer bin-nitrile/cleanide
+#       over bin-nitrile/prj/windows-x64/CleanIDE.exe on windows
+export PATH="$script_dir/bin-nitrile:$script_dir/bin-nitrile/prj/$PLATFORM_ARCH:$PATH"
